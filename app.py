2ecf7d42564bd8e06826ea550b57cc3e92503bbf
John2013 <NaWasington@yandex.ru>
2018-02-14 16:27:10 +0300

    flask app created

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, abort, jsonify
+from diff import text_diff
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+@app.route('/api/v1.0/diff', methods=['POST'])
+def diff():
+    if not request.json:
+        abort(400)
+
+    return jsonify({
+        'result': text_diff(
+            request.json["doc1"],
+            request.json["doc2"],
+            request.json['config']
+        )
+    }), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
