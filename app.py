e5c566a9165978090fa39c327678f62bb384c499
John2013 <NaWasington@yandex.ru>
2018-02-22 19:58:57 +0300

    добавлен фласк сервер с апи и страницей сравнения добавлен нормальная функция, расставляющая строки во втором тексте как в 1-ом

--- app.py
+++ app.py
@@ -1,73 +1,71 @@
-from flask import Flask, request, abort, jsonify
+from flask import Flask, render_template, request, abort, jsonify
+from os import remove
+from werkzeug.utils import secure_filename
+import logging
+from logging.handlers import RotatingFileHandler
+
 from diff import text_diff
 
 app = Flask(__name__)
+# app.config.from_object('config')
 
 
-@app.route("/")
+@app.route('/')
+@app.route('/index')
 def index():
-    text = "<h1>Get difference of 2 html documents</h1>\n" \
-        "<pre>POST http://127.0.0.1:5000/api/v1.0/diff\n" \
-        "Content-Type: application/json\n" \
-        "\n" \
-        "Params:\n" \
-        "{\n" \
-        "    \"doc1\": \"text1\",\n" \
-        "    \"doc2\": \"text2\",\n" \
-        "    \"config\": {//any config param are not required\n" \
-        "       \"deleted_element\": \"del\",\n" \
-        "        \"inserted_element\": \"ins\",\n" \
-        "        \"modified_class\": \"diff modified\",\n" \
-        "        \"deleted_class\": \"diff deleted\",\n" \
-        "        \"inserted_class\": \"diff inserted\",\n" \
-        "    }\n" \
-        "}</pre>\n" \
-        "<h2>Example</h2>\n" \
-        "<h3>Request</h3>\n" \
-        "<pre>POST http://127.0.0.1:5000/api/v1.0/diff\n" \
-        "Content-Type: application/json\n" \
-        "\n" \
-        "{\n" \
-        "    \"doc1\": \"&lt;ul&gt;\n\t&lt;li&gt;Автор: Григорьев П.А." \
-        "&lt;/li&gt;\n\t&lt;li&gt;Сумма: 126000 руб.&lt;/li&gt;\n\t&lt;li" \
-        "&gt;Дата: 26.12.14&lt;/li&gt;\n&lt;/ul&gt;\",\n" \
-        "    \"doc2\": \"&lt;ul&gt;\n\t&lt;li&gt;Сумма: 126000 руб.&lt;/li" \
-        "&gt;\n\t&lt;li&gt;Автор: Петров Г.Е.&lt;/li&gt;" \
-        "\n\t&lt;li&gt;Дата: 26.12.14&lt;/li&gt;\n\t484\n&lt;/ul&gt;\",\n" \
-        "    \"config\": {\n" \
-        "        \"modified_class\": \"diff modifieddd\",\n" \
-        "        \"deleted_class\": \"diff deleteddd\",\n" \
-        "        \"inserted_class\": \"diff inserteddd\"\n" \
-        "    }\n" \
-        "}</pre>\n" \
-        "<h3>Response</h3>\n" \
-        "<pre>{\n" \
-        "  \"result\": \"&lt;ul&gt;&lt;del class=\&quot;diff deleteddd" \
-        "\&quot;&gt;\n\t&lt;li&gt;\u0410\u0432\u0442\u043e\u0440: \u0413" \
-        "\u0440\u0438\u0433\u043e\u0440\u044c\u0435\u0432 \u041f.\u0410." \
-        "&lt;/li&gt;&lt;/del&gt;\n\t&lt;li&gt;\u0421\u0443\u043c\u043c\u0430" \
-        ": 126000 \u0440\u0443\u0431.&lt;/li&gt;\n\t&lt;li&gt;&lt;ins " \
-        "class=\&quot;diff inserteddd\&quot;&gt;\u0410\u0432\u0442\u043e" \
-        "\u0440: \u041f\u0435\u0442\u0440\u043e\u0432 \u0413.\u0415.&lt;/li" \
-        "&gt;\n\t&lt;li&gt;&lt;/ins&gt;\u0414\u0430\u0442\u0430: 26.12.14&l" \
-        "t;/li&gt;\n&lt;ins class=\&quot;diff inserteddd\&quot;&gt;\t484\n&" \
-        "lt;/ins&gt;&lt;/ul&gt;\"\n}</pre>"
-    return text
+    return render_template("index.html")
+
+
+@app.route("/api")
+@app.route("/api/help")
+def api_help():
+    return render_template("help.html")
 
 
 @app.route("/api/v1.0/diff", methods=["POST"])
-def diff():
+def api_diff():
     if not request.json:
         abort(400)
 
-    return jsonify({
-        "result": text_diff(
-            request.json["doc1"],
-            request.json["doc2"],
-            request.json["config"]
-        )
-    }), 200
+    try:
+        return jsonify({
+            "result": text_diff(
+                request.json["doc1"],
+                request.json["doc2"],
+                request.json["config"]
+            )
+        }), 200
+    except KeyError:
+        return jsonify({
+            "result": text_diff(
+                request.json["doc1"],
+                request.json["doc2"]
+            )
+        }), 200
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/api/v1.0/get_file_text", methods=["POST"])
+def get_file_text():
+    # return "111", 200
+    app.logger.info('111')
+    app.logger.info(request.files)
+    if "file" not in request.files:
+        app.logger.info('"file" not in request.files')
+        abort(400)
+    file = request.files['file']
+    app.logger.info('222')
+    filename = secure_filename(file.filename)
+    app.logger.info('333')
+    with open(filename) as file:
+        app.logger.info('444')
+        text = file.read()
+    app.logger.info('555')
+    remove(filename)
+    return text, 200
+
+
+if __name__ == '__main__':
+    handler = RotatingFileHandler('log.txt', maxBytes=10000, backupCount=1)
+    # handler.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
+    app.run(debug=True)
--- diff.py
+++ diff.py
@@ -3,10 +3,78 @@
 Rough code, badly documented. Send me comments and patches."""
 import difflib
 import string
+from operator import itemgetter
 
 
-def text_diff(text_from, text_to, config=None):
+def get_equality(str1: str, str2: str)->float:
+
+    if len(str1) != len(str2):
+        small_str = min(str1, str2)
+        big_str = max(str1, str2)
+    else:
+        small_str = str1
+        big_str = str2
+
+    equal_letters_count = 0
+    for number, letter in enumerate(small_str):
+        try:
+            if letter == big_str[number]:
+                equal_letters_count += 1
+            else:
+                break
+        except IndexError:
+            pass
+
+    letters_count = max(len(str1), len(str2))
+    return equal_letters_count / letters_count
+
+
+def get_max_equality_string_number(string1, text2_str_list, min_index=0):
+    equality_map = []
+    for index2, string2 in enumerate(text2_str_list):
+        equality_map.append(
+            (index2, get_equality(string1, string2))
+        )
+    most_similar_index, equality = max(equality_map, key=itemgetter(1))
+    if equality == 0:
+        return None
+    else:
+        return most_similar_index
+
+
+def similize(text1, text2):
+    text1_str_list = text1.split('\n')
+    text2_str_list = text2.split('\n')
+    index1_shift = 0
+    for index in range(max(len(text1_str_list), len(text2_str_list))):
+        try:
+            str1 = text1_str_list[index + index1_shift]
+        except IndexError:
+            continue
+        if str1 == text2_str_list[index]:
+            continue
+        else:
+            most_similar_string_index = get_max_equality_string_number(
+                str1,
+                text2_str_list,
+                index
+            )
+            if most_similar_string_index is None:
+                index1_shift += 1
+                continue
+            elif index > most_similar_string_index:
+                text2_str_list[index + index1_shift], text2_str_list[
+                    most_similar_string_index
+                ] = text2_str_list[most_similar_string_index], text2_str_list[
+                    index + index1_shift
+                ]
+    return "\n".join(text2_str_list)
+
+
+def text_diff(text1, text2, config=None):
     """Takes in strings a and b and returns a human-readable HTML diff."""
+
+    text2 = similize(text1, text2)
 
     config_default = {
         "deleted_element": "del",
@@ -24,16 +92,16 @@ def text_diff(text_from, text_to, config=None):
             config[key] = default_value
 
     out = []
-    text_from, text_to = html2list(text_from), html2list(text_to)
+    text1, text2 = html2list(text1), html2list(text2)
     try:
         matcher = difflib.SequenceMatcher(
             None,
-            text_from,
-            text_to,
+            text1,
+            text2,
             autojunk=False
         )
     except TypeError:
-        matcher = difflib.SequenceMatcher(None, text_from, text_to)
+        matcher = difflib.SequenceMatcher(None, text1, text2)
     for action, from_1, to_1, from_2, to_2 in matcher.get_opcodes():
         if action == "replace":
             # @@ need to do something more complicated here
@@ -45,14 +113,14 @@ def text_diff(text_from, text_to, config=None):
                     del_el=config["deleted_element"],
                     ins_el=config["inserted_element"],
                     mod_class=config["modified_class"],
-                    del_str=''.join(text_from[from_1:to_1]),
-                    ins_str=''.join(text_to[from_2:to_2])
+                    del_str=''.join(text1[from_1:to_1]),
+                    ins_str=''.join(text2[from_2:to_2])
                 )
             )
         elif action == "delete":
             out.append(
                 '<{del_el} class="{del_class}">{del_str}</{del_el}>'.format(
-                    del_str=''.join(text_from[from_1:to_1]),
+                    del_str=''.join(text1[from_1:to_1]),
                     del_el=config["deleted_element"],
                     del_class=config["deleted_class"]
                 )
@@ -60,7 +128,7 @@ def text_diff(text_from, text_to, config=None):
         elif action == "insert":
             out.append(
                 '<{ins_el} class="{ins_class}">{ins_str}</{ins_el}>'.format(
-                    ins_str=''.join(text_to[from_2:to_2]),
+                    ins_str=''.join(text2[from_2:to_2]),
                     ins_el=config["inserted_element"],
                     ins_class=config["inserted_class"]
                 )
@@ -68,7 +136,7 @@ def text_diff(text_from, text_to, config=None):
         elif action == "equal":
             out.append(
                 ''.join(
-                    text_to[from_2:to_2]
+                    text2[from_2:to_2]
                 )
             )
         else:
@@ -133,6 +201,8 @@ if __name__ == '__main__':
     text1 = open(a, encoding='utf-8').read()
     text2 = open(b, encoding='utf-8').read()
     print(text1, text2, sep='\n')
+    print("new text 2:")
+    print(text2)
     print(
         text_diff(
             text1,
