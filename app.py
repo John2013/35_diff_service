3da274309df00ce0404ec366d5d1c35c6b083bb1
John2013 <NaWasington@yandex.ru>
2018-02-22 20:05:22 +0300

    убрана функция сервера, возвращающая текст загружаемого файла, теперь этим занимается file api на фронте

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from flask import Flask, render_template, request, abort, jsonify
-from os import remove
-from werkzeug.utils import secure_filename
-import logging
 from logging.handlers import RotatingFileHandler
 
 from diff import text_diff
@@ -44,28 +41,7 @@ def api_diff():
         }), 200
 
 
-@app.route("/api/v1.0/get_file_text", methods=["POST"])
-def get_file_text():
-    # return "111", 200
-    app.logger.info('111')
-    app.logger.info(request.files)
-    if "file" not in request.files:
-        app.logger.info('"file" not in request.files')
-        abort(400)
-    file = request.files['file']
-    app.logger.info('222')
-    filename = secure_filename(file.filename)
-    app.logger.info('333')
-    with open(filename) as file:
-        app.logger.info('444')
-        text = file.read()
-    app.logger.info('555')
-    remove(filename)
-    return text, 200
-
-
 if __name__ == '__main__':
     handler = RotatingFileHandler('log.txt', maxBytes=10000, backupCount=1)
-    # handler.setLevel(logging.INFO)
     app.logger.addHandler(handler)
     app.run(debug=True)
