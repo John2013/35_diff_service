f6e6277567666251651b3c7ad4b3d79661082c9c
Evgeny Timofeyev <nawasington@ya.ru>
2018-11-24 20:09:47 +0300

    добавлены обработки ошибок

--- similize.py
+++ similize.py
@@ -1,4 +1,7 @@
 def swap(list, index1, index2):
+    if index1 not in list or index2 not in list:
+        return list
+
     list[index1], list[index2] = list[index2], list[index1]
     return list
 
@@ -8,7 +11,10 @@ def similize(text1, text2):
     text2_lines = text2.splitlines(True)
 
     for text1_line_number, text1_line in enumerate(text1_lines):
-        if text1_line == text2_lines[text1_line_number]:
+        try:
+            if text1_line == text2_lines[text1_line_number]:
+                continue
+        except IndexError:
             continue
 
         for text2_line_number, text2_line in enumerate(text2_lines):
@@ -19,11 +25,8 @@ def similize(text1, text2):
                 text1_lines = \
                     swap(text1_lines, text1_line_number, text2_line_number)
 
-    return text1_lines
+    return "".join(text1_lines)
 
 
 if __name__ == '__main__':
-    text1 = '111\n222\n333\n444\n555'
-    text2 = '111\n333\n222\n555\n441'
-
-    print(similize(text1, text2))
+    print(similize("111\n222\n333\n444\n555", "111\n222\n2233\n333\n444\n555"))
